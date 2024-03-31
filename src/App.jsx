d6a5c1846555b541feb55fab1907dcffe6596dd1
niru9711 <nirusanant97@gmail.com>
2024-03-31 12:08:51 +0530

    fetchData

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 /* Images */
@@ -10,6 +10,8 @@ import rainIcon from "./assets/rain.png";
 import windIcon from "./assets/wind2.png";
 import snowIcon from "./assets/snow.png";
 import humidityIcon from "./assets/humidity.png";
+import thunderstormIcon from "./assets/thunderstorm.png";
+import mistIcon from "./assets/mist.png";
 
 const WeatherDetails = ({ icon, temp, city, country, lat, long, humidity, wind }) => {
   return (
@@ -53,14 +55,90 @@ const WeatherDetails = ({ icon, temp, city, country, lat, long, humidity, wind }
 };
 
 function App() {
+  let api_key = "###";
+  const [text, setText] = useState('Colombo');
   const [icon, setIcon] = useState(cloudIcon);
   const [temp, setTemp] = useState(0);
   const [city, setCity] = useState('Colombo');
-  const [lat, setLat] = useState(12221);
+  const [lat, setLat] = useState(6.9319);
   const [long, setLong] = useState(34231);
-  const [country, setCountry] = useState('SL');
+  const [country, setCountry] = useState('LK');
   const [humidity, setHumidity] = useState(0);
   const [wind, setWind] = useState(0);
+
+  const [cityNotFound, setCityNotFound] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const weatherIconMap = {
+    "01d": clearIcon,
+    "01n": clearIcon,
+    "02d": cloudIcon,
+    "02n": cloudIcon,
+    "03d": drizzleIcon,
+    "03n": drizzleIcon,
+    "04d": drizzleIcon,
+    "04n": drizzleIcon,
+    "09d": rainIcon,
+    "09n": rainIcon,
+    "10d": rainIcon,
+    "10n": rainIcon,
+    "11d": thunderstormIcon,
+    "11n": thunderstormIcon,
+    "13d": snowIcon,
+    "13n": snowIcon,
+    "50d": mistIcon,
+    "50n": mistIcon,
+  }
+
+
+  const search = async () =>{
+    setLoading(true);
+    let url= `https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${api_key}&units=Metric`;
+
+    try{
+      let res = await fetch(url);
+      let data = await res.json();
+      // console.log(data);
+
+      if(data.cod === "404"){
+        console.error("City not found");
+        setCityNotFound(true);
+        setLoading(false);
+        return;
+      }
+
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setTemp(Math.floor(data.main.temp));
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setLat(data.coord.lat);
+      setLong(data.coord.lon);
+      const weatherIconCode = data.weather[0].icon;
+      setIcon(weatherIconMap[weatherIconCode] || clearIcon);
+      setCityNotFound(false);
+
+    }catch (error){
+      console.error("An error occurred:", error.message);
+      setError("An error occurred while fetching weather data");
+    } finally{
+      setLoading(false);
+    }
+  };
+
+  const handleCity= (e) => {
+    setText(e.target.value);
+  };
+  const handleKeyDown = (e) =>{
+    if(e.key === 'Enter'){
+      search();
+    }
+  };
+
+  useEffect(() =>{
+    search();
+  }, [])
 
   return (
     <>
@@ -68,14 +146,22 @@ function App() {
         <div className='input-container'>
           <input type='text'
             className='cityInput'
-            placeholder='search-city' />
-          <div className='search-icon'>
+            placeholder='search-city' 
+            onChange={handleCity}
+            value={text}
+            onKeyDown={handleKeyDown}/>
+          <div className='search-icon' onClick={()=>search()} >
             <img src={searchIcon} alt='Search' />
           </div>
         </div>
+ 
+        {loading && <div className='load-message'>Loading...</div>}
+        {error && <div className='error-message'>{error}</div>}
+        {cityNotFound && <div className='cityNotFound'>City not found</div>}
 
-        <WeatherDetails icon={icon} temp={temp} city={city} country={country} lat={lat} long={long} humidity={humidity} wind={wind}/>
-        
+        {!loading && !cityNotFound && <WeatherDetails icon={icon} temp={temp} city={city} country={country} lat={lat} long={long} humidity={humidity} wind={wind}/>}
+
+
         <p className='copyright'>
           Designed By <span>Niru</span>
         </p>
